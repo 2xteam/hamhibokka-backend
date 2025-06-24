822e523fb73cb9a4cb19cb623c934afb2e7a653e
2xteam <2xteam@naver.com>
2025-06-24 21:13:54 +0900

    Add csrf설정 변경경

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,6 +17,7 @@ import { AuthModule } from './auth/auth.module';
       playground: true, // 프로덕션에서도 playground 활성화
       introspection: true, // 스키마 조회 허용
       context: ({ req }) => ({ req }),
+      csrfPrevention: false,
     }),
     AuthModule,
   ],
